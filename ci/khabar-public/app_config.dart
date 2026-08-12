2bce93324da4c86b651d59eefef5965dbb290c80
ahbe0911-cmd <ahbe0911@gmail.com>
2026-08-12 23:48:27 +0330

    Add public web app config

---
+++ ci/khabar-public/app_config.dart
@@ -0,0 +1,5 @@
+abstract final class AppConfig {
+  static const String appName = 'خبر کافی‌نت';
+  static const String versionLabel = 'نسخه عمومی بدون ورود ۰.۴.۰';
+  static const String publicTelegramBaseUrl = 'https://t.me/s';
+}
